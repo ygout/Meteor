83061b07f7f3da7ee803f39dd788b495c9eff744
Corvus <renaud.cayol@gmail.com>
2016-06-03 14:22:21 +0200

    Clean code (for real)

--- meteorProject/client/main.js
+++ meteorProject/client/main.js
@@ -1,7 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { Comments } from '../imports/api/comments.js';
-import { Devs } from '../imports/api/devs.js';
 import '../imports/configs/at_configs.js';
 import '../imports/configs/routes.js';
 
@@ -13,6 +11,3 @@ import '../imports/ui/layout.html';
 import '../imports/ui/helpers.js';
 
 BlazeLayout.setRoot('body');
-
-// Meteor.subscribe('getComments');
-// Meteor.subscribe('getComment');
