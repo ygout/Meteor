b2a6b05b5a91188426b30c094bc6f2e6db540dbb
stagiaire <stagiaire@iMac17.local>
2016-07-01 16:56:49 +0200

    First try to implement post sing up hook

--- meteorProject/server/main.js
+++ meteorProject/server/main.js
@@ -6,4 +6,18 @@ import '../imports/configs/at_configs.js';
 Meteor.startup(() => {
   //Meteor.importDb();
   // code to run on server at startup
+  ServiceConfiguration.configurations.update(
+    { "service": "google" },
+    {
+      $set: {
+        "appId": "609274823758-i6mdo55bkeeal3vfqjjbrrsultspg4ri.apps.googleusercontent.com",
+        "secret": "jFJP0hLTY3m2Cx3nioNoJ3e8"
+      }
+    });
+    Accounts.onCreateUser(user) {
+      console.log('CREATE : ', user);
+    }
+    Accounts.onLogin(user) {
+      console.log('LOGIN : ', user);
+    }
 });
